10bf24eec63f2b00dfa0434b8eaf0eb92c1a5228
silly <>
2023-02-20 06:20:22 +0800

    footer 作成

--- footer.php
+++ footer.php
@@ -1,4 +1,33 @@
 <footer>
+  <div class="foot-top">
+    <ul>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">お問い合わせ</a></li>
+      <li><a href="#">利用規約</a></li>
+      <li><a href="#">プライバシーポリシー</a></li>
+      <li><a href="#">特定商取引法に基づく表記</a></li>
+    </ul>
+  </div>
+
+  <div class="foot-bottom ">
+    <ul class="foot-left">
+      <li><a href="#"><img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.shared__logo_wh.svg"
+            alt=""></a></li>
+      <li>©︎2022 FruOats</li>
+    </ul>
+
+    <ul class="foot-right">
+      <li class="oficial"><a href="#">Official Account</a></li>
+      <li><a href="#"><img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.shared__icon-twitter_wh.svg"
+            alt=""></a></li>
+      <li><a href="#"><img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.shared__icon-instagram_wh.svg"
+            alt=""></a></li>
+
+    </ul>
+  </div>
 
 </footer>
 
