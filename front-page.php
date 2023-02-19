77c9b2370072128c5621a314d4a8a59c978bd11e
silly <>
2023-02-20 00:01:51 +0800

    アーティクル作成

--- front-page.php
+++ front-page.php
@@ -74,9 +74,9 @@
       <img src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.index__scroll.svg" alt="">
     </div>
 
-    <div class="p-index-float">
-      <img src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/banner_1_400x.webp?ver=20230127"
-        alt="">
+    <div class="p-index-float"><a href="">
+        <img src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/banner_1_400x.webp?ver=20230127"
+          alt=""></a>
     </div>
 
     <section class="about">
@@ -105,12 +105,78 @@
         </p>
       </div>
     </section>
+  </div>
 
-    <div class="para">
-
-    </div>
+  <div class="para">
 
   </div>
+  <p class="para-text">
+    Strawberry.Pistachio. Mango. Coconut. Apple. Cinnamon. Fig. Almond. Orange. Chocolate.
+  </p>
+
+  <div class="container">
+
+
+    <section class="article-box">
+      <h3>About FruOats</h3>
+
+      <div class="article-wrapper">
+
+        <article>
+          <span>1</span>
+
+          <img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.index__feature_1.webp?ver=20230127"
+            alt="">
+          <div class="article-box1">
+            <h2>植物由来の素材で<br>
+              からだの中からキレイに</h2>
+            <p>
+              お菓子の材料によく使われる卵やバター、小麦粉、白砂糖は使用せず、オートミール、おからパウダー、チアシード、アマニ、ドライフルーツ、米油など、原材料は全て植物由来の素材を使用しています。また、人工甘味料や保存料も使用せず、からだにやさしいクッキーに仕上げました。
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <span>2</span>
+
+          <img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.index__feature_2.webp?ver=20230127"
+            alt="">
+          <div class="article-box1">
+            <h2>3枚で1/2日分の食物繊維<br>
+              糖質も約51%OFF</h2>
+            <p>
+              通常のクッキーと比べて栄養価は一目瞭然！低糖質で食物繊維がたっぷり。ほとんどの日本人が食物繊維不足で、20代~30代の男女で平均約6g不足しています。フルオーツならたった3枚(45g)で、1日に必要な食物繊維の1/2が摂れます。これは、バナナ約9~10本相当分です。
+            </p>
+          </div>
+        </article>
+
+        <article>
+          <span>3</span>
+
+          <img
+            src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.index__feature_3.webp?ver=20230127"
+            alt="">
+          <div class="article-box1">
+            <h2>現役パティシエ監修により<br>
+              満足度95%※の美味しさを実現！</h2>
+            <p>
+              美味しく食べていただくために、「クープ・デュ・モンド・ドゥ・ラ・パティスリー」をはじめとする、パティシエの国際大会で優勝経験のある中山和大シェフに監修して頂きました。美味しい健康食品を作ることの難しさを痛感しながらも、栄養素や素材などに一切妥協せず、美味しく作り上げるために幾度の試行錯誤を重ねました。
+              （※2021年9月調査 モニター41人）</p>
+          </div>
+        </article>
+      </div>
+    </section>
+
+    <section class="story">
+
+
+    </section>
+
+  </div>
+
+
 </main>
 
 <?php get_footer(); ?>
