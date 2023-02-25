4eac1d1d45fec34079a2a9779d435b1ffdd36381
silly <>
2023-02-25 17:35:40 +0800

    news archive single 作成

--- archive.php
+++ archive.php
@@ -1,5 +1,44 @@
 <?php get_header(); ?>
 
+<div class="news-wrap">
 
+
+  <div class="container">
+
+    <h1>NEWS</h1>
+
+    <section id="news" class="newsMove1 newsarea">
+
+
+      <dl>
+
+        <?php
+      $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 50,
+      'order' => 'DESC',
+      );
+      $query_instance = new WP_Query($args);
+      ?>
+        <?php if ($query_instance->have_posts()): ?>
+        <?php while ( $query_instance->have_posts()): $query_instance->the_post(); ?>
+
+        <a href="<?php echo get_permalink( $id );?>" class="newslink">
+          <dt><?php the_time('Y.n.j'); ?></dt>
+          <dd><?php the_title(); ?></dd>
+        </a>
+
+        <?php endwhile ?>
+        <?php endif ?>
+        <?php wp_reset_postdata(); ?>
+
+      </dl>
+
+
+    </section>
+
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,5 +1,47 @@
 <?php get_header(); ?>
 
 
+<div class="single-wrap">
+  <div class="container">
+
+    <div class="single-inner">
+
+      <?php if(have_posts()): while(have_posts()): the_post();?>
+
+      <h1>
+        <?php the_title(); ?>
+      </h1>
+
+      <h3>
+        <?php the_time('Y.n.j'); ?>
+      </h3>
+
+      <img src="<?php the_field('news画像1'); ?>" alt="">
+      <p>
+        <?php the_content(); ?>
+      </p>
+
+      <?php endwhile; endif; ?>
+
+
+      <div class="single-sns">
+        <h4>SHARE</h4>
+        <ul>
+          <li><img
+              src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.shared__icon-twitter_or.svg"
+              alt=""></li>
+          <li><img
+              src="https://d2w53g1q050m78.cloudfront.net/shopfruoatsjp/uploads/assets/static.shared__icon-facebook_or.svg"
+              alt=""></li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
+
+  <a href="<?php echo home_url('/'); ?>news/" class="top-btn">NEWS一覧ページへ</a>
+
+</div>
+
 
 <?php get_footer(); ?>
